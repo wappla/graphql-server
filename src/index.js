aafceed959ddbdee1298e9e8e7615d0f56f8d55f
Dries Vandermeulen <dries.vandermeulen1@gmail.com>
2021-04-21 10:54:48 +0200

    Add graphql types

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+import {
+    GraphQLDate as Date,
+    GraphQLTime as Time,
+    GraphQLDateTime as DateTime,
+} from 'graphql-iso-date'
+import { GraphQLUpload as Upload } from 'graphql-upload'
+
 export { default as createGraphqlRequestHandler } from './createGraphqlRequestHandler'
 export { default as GraphqlQueryStore } from './GraphqlQueryStore'
 export * from './errors'
+export const types = {
+    Upload,
+    Date,
+    Time,
+    DateTime,
+}
